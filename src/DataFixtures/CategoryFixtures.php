f7659193c0cc1cbd5167f81c712f77af53c321c2
Coco Pavoine <cpavoine@gmail.com>
2019-05-20 16:55:03 +0200

    Création Fixtures User

--- src/DataFixtures/CategoryFixtures.php
+++ src/DataFixtures/CategoryFixtures.php
@@ -6,6 +6,7 @@ use App\Entity\Category;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
+
 class CategoryFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
